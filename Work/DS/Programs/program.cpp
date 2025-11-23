0b1c281f73467e7c357b1bbb7e0fed64d1c1005b
param-punjab <matharooparam@gmail.com>
2025-11-23 23:07:49 +0530

    through mam

--- Work/DS/Programs/program.cpp
+++ Work/DS/Programs/program.cpp
@@ -33,34 +33,46 @@ Node* Create() {
 void TraLRoRi(Node* root) {
   Node* ptr = new Node;
   Node* ptr_2 = new Node;
+
   if ( (*root).left != nullptr) {
     ptr = (*root).left;
     if ( (*ptr).left != nullptr ) {
-      ptr_2 == (*ptr).left;
+      ptr_2 = (*ptr).left;
       if ((*ptr_2).left != nullptr) { TraLRoRi(ptr_2);
       } else { cout << ptr_2->data << endl; }
     }
+
     cout << (*ptr).data << endl;
     if ( (*ptr).right != nullptr ) {
       Node* ptr_r = new Node;
       ptr_r = (*ptr).right;
       TraLRoRi(ptr_r);
     } 
+
     cout << (*root).data << endl;
   } else if ( (*root).right != nullptr ) {
     Node* root_ptr_r = new Node;
     root_ptr_r = (*root).right;
-    if ( root_ptr_r->left != nullptr) { TraLRoRi(root_ptr_r); }
+    if ( root_ptr_r->left != nullptr) { 
+      Node* new_ptr = new Node;
+      new_ptr = root_ptr_r->left;
+      TraLRoRi(new_ptr);
+    }
     cout << root_ptr_r->data << endl;
+    if (root_ptr_r->right != nullptr) { 
+      Node* new_ptr_r = new Node;
+      new_ptr_r = root_ptr_r->right;
+      TraLRoRi(new_ptr_r);
+    }
   }
 }
 
-void Display() {
-
-}
 
 int main() {
+  cout << "I am Param" << endl;
   Node* root;
-  root =  Create();
+  root = Create();
+  cout << "Printing Elements In Order" << endl;
+  TraLRoRi(root);
   return 0;
 }
